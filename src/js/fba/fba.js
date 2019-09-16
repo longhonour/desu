f307765e8d807a5ac099a13a0533e08f51986fff
曹龙 <2286410840@qq.com>
2019-09-17 00:48:14 +0800

    add # fba相关介绍

--- src/js/fba/fba.js
+++ src/js/fba/fba.js
@@ -1,3 +1,17 @@
 import '../../less/fba/fba.less';
 
 import '../common/common';
+
+$(function () {
+    $('.fba-nav').on('click', 'span', function () {
+        if ($(this).hasClass('active')) {
+            return;
+        }
+        var hrefId = $(this).attr('href');
+        if ($(hrefId).length > 0) {
+            $(this).addClass('active').siblings('span').removeClass('active');
+            $('.fba-content .content-item').hide();
+            $(hrefId).fadeIn(300);
+        }
+    })
+});
