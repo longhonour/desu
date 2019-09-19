7b5a2df06ffdad71e5f84b877b1f1cafed563005
曹龙 <2286410840@qq.com>
2019-09-20 01:03:09 +0800

    Changed # 首页修改

--- src/js/fba/fba.js
+++ src/js/fba/fba.js
@@ -6,17 +6,24 @@ $(function () {
     var $sideNav = $('.fba-nav');
     var scrollTimer = null;
     var navTop = $sideNav.offset().top;
-    $sideNav.on('click', 'span', function () {
-        if ($(this).hasClass('active')) {
-            return;
-        }
-        var hrefId = $(this).attr('href');
-        if ($(hrefId).length > 0) {
-            $(this).addClass('active').siblings('span').removeClass('active');
-            $('.fba-content .content-item').hide();
-            $(hrefId).fadeIn(300);
-        }
-    });
+    var achorNav = getUrlParam('nav');
+    var achorTop = achorNav ? $('#' + achorNav).offset().top - 80 : 0;
+
+    function getUrlParam(paramKey, urlSearch) {
+        paramKey = paramKey || '';
+        let val = '';
+        let query = urlSearch || window.location.search || '';
+        query = query[0] === '?' ? query.slice(1) : query;
+        let params = query.split('&');
+        params.forEach(function (item) {
+            item = item || '';
+            let kv = item.split('=');
+            if (kv && kv.length === 2 && kv[0] === paramKey) {
+                val = kv[1];
+            }
+        });
+        return val;
+    }
 
     function setSideNavFixed() {
         var cTop = $(window).scrollTop();
@@ -26,6 +33,23 @@ $(function () {
             $sideNav.addClass('fixed');
         }
     }
+
+    $sideNav.on('click', 'span', function () {
+        if ($(this).hasClass('active')) {
+            return;
+        }
+        var hrefId = $(this).attr('href');
+        if ($(hrefId).length > 0) {
+            $(this).addClass('active').siblings('span').removeClass('active');
+            $('.fba-content .content-item').hide();
+            $(hrefId).fadeIn(300);
+            $('body, html').animate({
+                scrollTop: navTop - 100
+            }, 200);
+        }
+    });
+
+    $(window).scrollTop(achorTop);
 
     setSideNavFixed();
 
--- src/js/home/home.js
+++ src/js/home/home.js
@@ -3,10 +3,11 @@ import '../../less/home/home.less';
 import '../common/common';
 
 new Swiper('.swiper-container', {
+    lazyLoading: true,
     pagination: '.swiper-pagination',
     paginationClickable: true,
     autoplay: 3000,
-    speed: 800,
+    speed: 600,
     spaceBetween: 0,
     effect: 'fade'
 });
